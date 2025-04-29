5cfe26dcb27c6e0581b626ea80a725ee9ee3f3f1
Rafael Carvalho <rafael_hosanah5@hotmail.com>
2025-04-29 16:36:19 -0300

    Atualização do layout para o primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,6 +14,11 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(
       withInterceptors([authInterceptor])
-    )
+    ),
+    providePrimeNG({ 
+      theme: {
+          preset: Aura
+      }
+    })
   ]
 };
